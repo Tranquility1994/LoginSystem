fb668515cf4366014a5e5934c4dab30cd7b307c7
marthijn kip <marthijn1994@gmail.com>
2017-01-14 16:55:29 +0100

    Added global config variable to get access to the needed data Also added autoload classes.

--- core/init.php
+++ core/init.php
@@ -5,3 +5,27 @@
  * Date: 14-1-2017
  * Time: 16:47
  */
+
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => '',
+        'db' => 'loginsystemdb'
+    ),
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ),
+    'session' => array(
+        'session_name' => 'user'
+    )
+);
+
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
--- index.php
+++ index.php
@@ -5,3 +5,5 @@
  * Date: 14-1-2017
  * Time: 16:41
  */
+
+require_once 'core/init.php';

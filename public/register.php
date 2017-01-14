c936e7574feb145aa42beff22b02d88ba797e4f9
marthijn kip <marthijn1994@gmail.com>
2017-01-14 22:22:35 +0100

    Created public folder to hold all the public files. Moved files to the public folder. Added Bootstrap. Added new folder called partials, will hold all the template partials.

---
+++ public/register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+</head>
+<body>
+
+<?php include_once '../includes/partials/header.php'; ?>
+
+<div class="container">
+    <div class="well">
+        <form action="" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Username">
+            </div>
+        </form>
+    </div>
+</div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- update.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marthijn
- * Date: 14-1-2017
- * Time: 16:44
- */

4aa5263a39287f92f305dabca321c4bdf424815e
marthijn kip <marthijn1994@gmail.com>
2017-01-15 22:03:53 +0100

    Added Input class to get easy access to the get & post data. Added Validate class to add validation to the forms.

--- classes/Input.php
+++ classes/Input.php
@@ -5,3 +5,30 @@
  * Date: 14-1-2017
  * Time: 16:46
  */
+
+class Input {
+
+    public static function exists($type = 'post') {
+        switch ($type) {
+            case 'post':
+                return (!empty($_POST)) ? true : false;
+                break;
+            case 'get':
+                return (!empty($_GET)) ? true : false;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    public static function get($item) {
+        if (isset($_POST[$item])) {
+            return $_POST[$item];
+        } else if(isset($_GET[$item])) {
+            return $_GET[$item];
+        }
+        return '';
+    }
+
+}
--- public/register.php
+++ public/register.php
@@ -1,3 +1,43 @@
+<?php
+include_once '../core/init.php';
+
+$passed = true;
+$errors = array();
+
+if (Input::exists()) {
+    $validate = new Validate();
+    $validation = $validate->check($_POST, array(
+        'username' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 20,
+            'unique' => 'users'
+        ),
+        'password' => array(
+            'required' => true,
+            'min' => 6
+        ),
+        'password_again' => array(
+            'required' => true,
+            'matches' => 'password'
+        ),
+        'name' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 50
+        )
+    ));
+
+    $passed = $validation->passed();
+    if (!$passed) {
+        $errors = $validation->errors();
+    } else {
+        echo "true";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -14,9 +54,41 @@
 
 <div class="container">
     <div class="well">
+        <?php
+        if (!$passed) { ?>
+            <div class="alert alert-danger">
+                <b>Fix the following errors to continue:</b>
+                <ul>
+                <?php
+                foreach($errors as $error) {
+                    echo "<li>$error</li>";
+                }
+                ?>
+                </ul>
+            </div>
+        <?php } ?>
         <form action="" method="post">
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Username">
+            <div class="input-group col-md-3">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" value="<?php echo escape(Input::get('username')); ?>">
+            </div>
+
+            <div class="input-group col-md-3">
+                <label for="name">Full Name</label>
+                <input type="text" class="form-control" name="name" value="<?php echo escape(Input::get('name')); ?>">
+            </div>
+
+            <div class="input-group col-md-3">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+
+            <div class="input-group col-md-3">
+                <label for="password_again">Password confirmation</label>
+                <input type="password" class="form-control" name="password_again">
+            </div>
+            <div class="btn-group ">
+                <input type="submit" value="Register" class="btn btn-primary">
             </div>
         </form>
     </div>
